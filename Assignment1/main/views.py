aae307ea6f96ce924cb477dd94262345f9cf2c03
AaronGr767 <aarongroarke2@gmail.com>
2022-11-08 13:51:37 +0000

    Added cookies to form to update db location

--- Assignment1/main/views.py
+++ Assignment1/main/views.py
@@ -20,7 +20,7 @@ def register_request(request):
 			user_profile.save()
 			login(request, user)
 			messages.success(request, "Registration successful." )
-			return redirect("main:login")
+			return redirect("login")
 		messages.error(request, "Unsuccessful registration. Invalid information.")
 	form = UserCreationForm()
 	return render(request=request, template_name="registration/register.html", context={"register_form":form})
@@ -56,6 +56,7 @@ def login_request(request):
 
 @login_required
 def update_database(request):
+
     my_location = request.POST.get("point", None)
     if not my_location:
         return JsonResponse({"message": "No location found."}, status=400)
